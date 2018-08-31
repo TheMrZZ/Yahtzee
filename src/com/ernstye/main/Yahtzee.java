a2135a7ad9d5e117e372730ac99442f92734b6fb
Florian ERNST <florianernst59@gmail.com>
2018-08-31 18:22:20 -0400

    Added TODO javadoc

--- src/com/ernstye/main/Yahtzee.java
+++ src/com/ernstye/main/Yahtzee.java
@@ -1,8 +1,18 @@
 package com.ernstye.main;
 
 public class Yahtzee {
+    /**
+     * A working Yahtzee implementation.
+     * <p>
+     * TODO:
+     * - Implement a single-player working version of the dices
+     * - Implement the upper section of the points
+     * </p>
+     */
 
     public static void main(String[] args) {
 
     }
+
+
 }
