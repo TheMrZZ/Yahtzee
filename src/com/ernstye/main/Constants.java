df8db9352bc72282feb6d28a394525cbb948d5fc
Florian ERNST <florianernst59@gmail.com>
2018-08-31 19:35:00 -0400

    Added constants for display

--- src/com/ernstye/main/Constants.java
+++ src/com/ernstye/main/Constants.java
@@ -1,8 +1,12 @@
 package com.ernstye.main;
 
+/**
+ * Collection of constants used for the game.
+ * <p>All members of this class are immutable.</p>
+ */
 public final class Constants
 {
-    // Used for the rows (in the score grid) the player didn't use for the moment
+    /** Used for the rows (in the score grid) the player didn't use for the moment */
     public static final int NO_SCORE = -1;
 
     // The number of faces a dice has
@@ -10,4 +14,10 @@ public final class Constants
 
     // The number of dices we play with
     public static final int NUMBER_OF_DICES = 5;
+
+    public static final String[] DICE_IMAGES = {"⚀", "⚁", "⚂", "⚃", "⚄", "⚅"};
+
+    public static final String[] UPPER_SECTION_ROWS = {
+            "Ones", "Twos", "Threes", "Fours", "Fives", "Sixes"
+    };
 }
