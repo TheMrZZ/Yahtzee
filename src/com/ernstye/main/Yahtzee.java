335cfc6bdcac12baa9c5785309d347373de14beb
Céline <celine.ye@efrei.net>
2018-08-31 19:30:31 -0400

    First random throw Display One turn

--- src/com/ernstye/main/Yahtzee.java
+++ src/com/ernstye/main/Yahtzee.java
@@ -1,6 +1,8 @@
 package com.ernstye.main;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 import static com.ernstye.main.Constants.*;
 
@@ -20,6 +22,53 @@ public class Yahtzee
         System.out.println("== YAHTZEE ==");
 
         int[] upperSection = createUpperSection();
+
+        int dices[]=startDices();
+        displayDices(dices);
+        dices=nextTurn(dices);
+        displayDices(dices);
+    }
+
+    public static int[] startDices()
+    {
+        int dices[]=new int[6];
+        System.out.println("YAHTZEE GAME !");
+        Random random= new Random();
+        System.out.println("Result of your toss :");
+
+        for (int i=0; i<6;i++)
+        {
+            dices[i]=random.nextInt(6)+1;
+
+        }
+        return dices;
+
+    }
+
+    public static int[] nextTurn(int dices[])
+    {
+        Random random=new Random();
+        Scanner scanner = new Scanner(System.in);
+        int dicesToRoll;
+        System.out.println("How many dices do you want to toss again");
+        dicesToRoll=scanner.nextInt();
+        System.out.println("Which dices would you like to throw again?");
+        for (int i=0;i<dicesToRoll;i++)
+        {
+            int throwAgainDice=scanner.nextInt();
+            dices[throwAgainDice]=random.nextInt(6)+1;
+        }
+        return dices;
+
+
+    }
+
+    public static void displayDices(int dices[])
+    {
+        for (int i=0; i<6;i++)
+        {
+            System.out.println(i+") "+ dices[i]);
+        }
     }
 
     // Create the empty upper section: 6 rows for each possible scores
