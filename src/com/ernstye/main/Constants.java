eda05fcd3d28bf5a48255760d9c67ed925169eaa
Florian ERNST <florianernst59@gmail.com>
2018-08-31 18:56:16 -0400

    Added constants: number of dices, number of faces for each dice, and no score

---
+++ src/com/ernstye/main/Constants.java
@@ -0,0 +1,13 @@
+package com.ernstye.main;
+
+public final class Constants
+{
+    // Used for the rows (in the score grid) the player didn't use for the moment
+    public static final int NO_SCORE = -1;
+
+    // The number of faces a dice has
+    public static final int DICE_FACES = 6;
+
+    // The number of dices we play with
+    public static final int NUMBER_OF_DICES = 5;
+}
