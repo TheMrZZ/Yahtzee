7503c0136b1e492b2bcd863d1cd31412123a772a
Céline <celine.ye@efrei.net>
2018-08-31 20:39:40 -0400

    Class Dices

--- src/com/ernstye/main/Yahtzee.java
+++ src/com/ernstye/main/Yahtzee.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 import static com.ernstye.main.Constants.*;
+import static com.ernstye.main.Dices.*;
 
 public class Yahtzee
 {
@@ -22,64 +23,16 @@ public class Yahtzee
         System.out.println("== YAHTZEE ==");
 
         int[] upperSection = createUpperSection();
-
-        int dices[]=startDices();
-        displayDices(dices);
+        Dices dices=new Dices();
+        dices.startDices();
+        dices.displayDices();
         for (int i=0; i<2;i++)
         {
             System.out.println("\nTURN "+(i+2));
-            nextTurn(dices);
-            displayDices(dices);
+            dices.nextTurn();
+            dices.displayDices();
         }
 
-    }
-
-    /*
-    First turn: the 5 dices are being tossed randomly
-     */
-    public static int[] startDices()
-    {
-        int dices[]=new int[NUMBER_OF_DICES];
-        Random random= new Random();
-        System.out.println("YAHTZEE GAME !");
-        System.out.println("TURN 1");
-
-        for (int i=0; i<NUMBER_OF_DICES;i++)
-        {
-            dices[i]=random.nextInt(DICE_FACES)+1;
-
-        }
-        return dices;
-
-    }
-
-    public static void nextTurn(int dices[])
-    {
-        Random random=new Random();
-        Scanner scanner = new Scanner(System.in);
-        int dicesToRoll;
-        System.out.println("How many dices do you want to toss again?");
-        dicesToRoll=scanner.nextInt();
-        System.out.println("Which dices would you like to toss again?");
-        for (int i=0;i<dicesToRoll;i++)
-        {
-            int throwAgainDice=scanner.nextInt();
-            //The dice chosen is being toss again
-            dices[throwAgainDice]=random.nextInt(DICE_FACES)+1;
-        }
-
-
-    }
-
-    /*
-    This method shows the toss of the 5 dices
-     */
-    public static void displayDices(int dices[])
-    {
-        for (int i=0; i<NUMBER_OF_DICES;i++)
-        {
-            System.out.println(i+") "+ dices[i]);
-        }
     }
 
     // Create the empty upper section: 6 rows for each possible scores
