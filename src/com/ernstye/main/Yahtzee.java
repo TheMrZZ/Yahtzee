6e3c4c6a142a40a530bbc5b07d14aa8bdf47d3de
Céline <celine.ye@efrei.net>
2018-08-31 20:00:40 -0400

    Constants

--- src/com/ernstye/main/Yahtzee.java
+++ src/com/ernstye/main/Yahtzee.java
@@ -25,47 +25,58 @@ public class Yahtzee
 
         int dices[]=startDices();
         displayDices(dices);
-        dices=nextTurn(dices);
-        displayDices(dices);
+        for (int i=0; i<2;i++)
+        {
+            System.out.println("\nTURN "+(i+2));
+            nextTurn(dices);
+            displayDices(dices);
+        }
+
     }
 
+    /*
+    First turn: the 5 dices are being tossed randomly
+     */
     public static int[] startDices()
     {
-        int dices[]=new int[6];
-        System.out.println("YAHTZEE GAME !");
+        int dices[]=new int[NUMBER_OF_DICES];
         Random random= new Random();
-        System.out.println("Result of your toss :");
+        System.out.println("YAHTZEE GAME !");
+        System.out.println("TURN 1");
 
-        for (int i=0; i<6;i++)
+        for (int i=0; i<NUMBER_OF_DICES;i++)
         {
-            dices[i]=random.nextInt(6)+1;
+            dices[i]=random.nextInt(DICE_FACES)+1;
 
         }
         return dices;
 
     }
 
-    public static int[] nextTurn(int dices[])
+    public static void nextTurn(int dices[])
     {
         Random random=new Random();
         Scanner scanner = new Scanner(System.in);
         int dicesToRoll;
-        System.out.println("How many dices do you want to toss again");
+        System.out.println("How many dices do you want to toss again?");
         dicesToRoll=scanner.nextInt();
-        System.out.println("Which dices would you like to throw again?");
+        System.out.println("Which dices would you like to toss again?");
         for (int i=0;i<dicesToRoll;i++)
         {
             int throwAgainDice=scanner.nextInt();
-            dices[throwAgainDice]=random.nextInt(6)+1;
+            //The dice chosen is being toss again
+            dices[throwAgainDice]=random.nextInt(DICE_FACES)+1;
         }
-        return dices;
 
 
     }
 
+    /*
+    This method shows the toss of the 5 dices
+     */
     public static void displayDices(int dices[])
     {
-        for (int i=0; i<6;i++)
+        for (int i=0; i<NUMBER_OF_DICES;i++)
         {
             System.out.println(i+") "+ dices[i]);
         }
