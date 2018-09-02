2686b3afb623261587168ae8b4a13328dfd94c87
Florian ERNST <florianernst59@gmail.com>
2018-09-01 20:00:16 -0400

    Added descriptions for the constants

--- src/com/ernstye/main/Constants.java
+++ src/com/ernstye/main/Constants.java
@@ -6,18 +6,29 @@ package com.ernstye.main;
  */
 final class Constants
 {
-    /** Used for the rows (in the score grid) the player didn't use for the moment */
+    /**
+     * Used for the rows (in the score grid) the player didn't use for the moment
+     */
     static final int NO_SCORE = -1;
 
-    // The number of faces a dice has
+    /**
+     * The number of faces a dice has
+     */
     static final int DICE_FACES = 6;
 
-    // The number of dices we play with
+    /**
+     * The number of dices we play with
+     */
     static final int NUMBER_OF_DICES = 5;
 
-    // Images of each dice face in unicode
+    /**
+     * Images of each dice face in unicode
+     */
     static final String[] DICE_IMAGES = {"\u2680", "\u2681", "\u2682", "\u2684", "\u2685", "\u2686"};
 
+    /**
+     * The names of every line in the upper section
+     */
     static final String[] UPPER_SECTION_ROWS = {
             "Ones", "Twos", "Threes", "Fours", "Fives", "Sixes"
     };
