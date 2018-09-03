ca734a2ac976b80f1e770d790b73a2ea8651e5d5
Florian ERNST <florianernst59@gmail.com>
2018-09-03 18:06:20 -0400

    Start now updates the java

--- src/com/ernstye/main/start.bat
+++ src/com/ernstye/main/start.bat
@@ -1,2 +1,4 @@
 @echo off
+javac -d "C:\Users\Florian\IdeaProjects\Yahtzee\out\production\Yahtzee" *.java
 java -classpath "C:\Users\Florian\IdeaProjects\Yahtzee\out\production\Yahtzee" com.ernstye.main.Yahtzee
+cmd /k
